6c0d1b22453067bdb46d3955fc9bd8e098a62261
Máté Cserép <mcserep@gmail.com>
2020-10-12 10:24:54 +0200

    Add CMake project compilation to ubuntu-2004-qt5.

--- ubuntu-2004-qt5/build.sh
+++ ubuntu-2004-qt5/build.sh
@@ -3,14 +3,24 @@
 set -e
 cwd=$(pwd)
 IFS=$'\n'
+
+counter=$(find . -iname "*.pro" | wc -l)
 find . -type f -name "*.pro" | while read file; do
     cd $(dirname $file)
     qmake $(basename $file)
     make
     cd $cwd
 done
-counter=$(find . -iname "*.pro" | wc -l)
+
+if [ $counter -eq 0 ] && [ -f "CMakeLists.txt" ]; then
+    counter=1
+    mkdir /build
+    cd /build
+    cmake $cwd
+    make
+fi
+
 if [ $counter -eq 0 ]; then
-    echo "No Qt projects found." 1>&2
+    echo "No Qt or CMake projects found." 1>&2
     exit 1
 fi
