2363b600309d96ee1ec3e253b2070a755146c5c8
Máté Cserép <mcserep@gmail.com>
2020-11-10 01:47:33 +0100

    Add ErrorActionPreference configuration to dotnet-48 image.

--- dotnet-48/build.ps1
+++ dotnet-48/build.ps1
@@ -1,11 +1,15 @@
+$ErrorActionPreference = "Stop"
 $count = (Get-ChildItem -Path .\ -Filter *.sln -Recurse | Group-Object -Property Directory).Count
 if ($count -gt 0) {
   Get-ChildItem -Path .\ -Filter *.sln -Recurse -File -Name | ForEach-Object {
     Write-Output "Now building: $_"
     dotnet build "$_"
+    
+    if ($LastExitCode -ne 0) {
+      Write-Error -Message "Compilation error." -Category InvalidOperation
+    }
   }
 }
 else {
   Write-Error -Message "No Visual Studio solutions found." -Category InvalidData
-  exit 1
 }
