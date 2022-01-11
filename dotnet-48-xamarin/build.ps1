a0db89a25671dac90fd2a0652f9769ae3837c155
Máté Cserép <mcserep@gmail.com>
2022-01-11 17:42:38 +0100

    Fixed path escaping in build script of dotnet-48-xamarin image.

--- dotnet-48-xamarin/build.ps1
+++ dotnet-48-xamarin/build.ps1
@@ -3,7 +3,7 @@ $count = (Get-ChildItem -Path .\ -Filter *.sln -Recurse | Group-Object -Property
 if ($count -gt 0) {
   Get-ChildItem -Path .\ -Filter *.sln -Recurse -File -Name | ForEach-Object {
     Write-Output "Now building: $_"
-    MSBuild $_ /restore /t:Build
+    MSBuild "$_" /restore /t:Build
     
     if ($LastExitCode -ne 0) {
       Write-Error -Message "Compilation error." -Category InvalidOperation
