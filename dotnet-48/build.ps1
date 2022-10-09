839ec11c63dd21e8da29e8ca462767a283189228
Máté Cserép <mcserep@gmail.com>
2022-10-09 21:17:21 +0200

    Use MsBuild in dotnet-48 image to support building .NET Framework projects.

--- dotnet-48/build.ps1
+++ dotnet-48/build.ps1
@@ -1,19 +1,10 @@
 $ErrorActionPreference = "Stop"
 $count = (Get-ChildItem -Path .\ -Filter *.sln -Recurse | Group-Object -Property Directory).Count
 if ($count -gt 0) {
-  Get-ChildItem -Path .\ -Filter *.csproj -Recurse -File -Name | ForEach-Object {
-    Write-Output "Now checking: $_"
-    $isDotnetFramework = (Select-String -Path "$_" -Pattern "Project ToolsVersion=").Matches.Success
-    
-    if ($isDotnetFramework) {
-      Write-Error -Message ".NET Framework project found. Use .NET (Core) instead." -Category InvalidType
-    }
-  }
-
-  Get-ChildItem -Path .\ -Filter *.sln -Recurse -File -Name | ForEach-Object {  
+  Get-ChildItem -Path .\ -Filter *.sln -Recurse -File -Name | ForEach-Object {
     Write-Output "Now building: $_"
-    dotnet build "$_"
-    
+    MSBuild "$_" /restore /t:Build
+
     if ($LastExitCode -ne 0) {
       Write-Error -Message "Compilation error." -Category InvalidOperation
     }
