15c6471846573dab8c199eb3448465604a9272e8
Máté Cserép <mcserep@gmail.com>
2022-10-09 20:51:43 +0200

    Check for .NET Framework project types in dotnet-48 image.

--- dotnet-48/build.ps1
+++ dotnet-48/build.ps1
@@ -1,7 +1,16 @@
 $ErrorActionPreference = "Stop"
 $count = (Get-ChildItem -Path .\ -Filter *.sln -Recurse | Group-Object -Property Directory).Count
 if ($count -gt 0) {
-  Get-ChildItem -Path .\ -Filter *.sln -Recurse -File -Name | ForEach-Object {
+  Get-ChildItem -Path .\ -Filter *.csproj -Recurse -File -Name | ForEach-Object {
+    Write-Output "Now checking: $_"
+    $isDotnetFramework = (Select-String -Path "$_" -Pattern "Project ToolsVersion=").Matches.Success
+    
+    if ($isDotnetFramework) {
+      Write-Error -Message ".NET Framework project found. Use .NET (Core) instead." -Category InvalidType
+    }
+  }
+
+  Get-ChildItem -Path .\ -Filter *.sln -Recurse -File -Name | ForEach-Object {  
     Write-Output "Now building: $_"
     dotnet build "$_"
     
