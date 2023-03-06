b57bdfc678882506c8fa023c3479101b50b472ac
Máté Cserép <mcserep@gmail.com>
2023-03-06 08:24:44 +0100

    Add execution script dotnet-60-linux image.

--- dotnet-60/linux/build.sh
+++ dotnet-60/linux/build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 set -e
-cwd=$(pwd)
 IFS=$'\n'
 
 counter=$(find . -iname "*.sln" | wc -l)
 find . -type f -name "*.sln" | while read file; do
     echo "Now building: $file"
     dotnet build $file
-    cd $cwd
 done
 
 if [ $counter -eq 0 ]; then
